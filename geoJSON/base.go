b9032bb9925068348479af70864bf307aae1c364
giulio <giulioungaretti@me.com>
2016-03-17 14:47:26 +0100

    Add GeoJson point.

--- geoJSON/base.go
+++ geoJSON/base.go
@@ -52,24 +52,47 @@ func (cc Coordinates) tos2() (s2.Loop, error) {
 	return *s2.LoopFromPoints(pts), nil
 }
 
+// Point rapresent a geoJSON point geometry object
+type Point struct {
+	Type       string `json:"type"`
+	Coordinate `json:"coordinates"`
+}
+
 // ToS2 converts lat/long to S2Cellid. If p.Precision is specified then the
 // parent cellid at specfied level is returned.
 // levels go from 0 to 30:
 // for reference :
-// 0 covers 0.48cm2
+// 30 covers 0.48cm2
 // 12 covers 3.31km2
-func (p Point) ToS2(precision *int) uint64 {
+// 0 covers 85,011,012 km2
+func (p Point) ToS2(precision int) [][]uint64 {
 	c := p.Coordinate
 	ll := s2.LatLngFromDegrees(c[1], c[0])
 	cellID := s2.CellIDFromLatLng(ll)
 	// approximate returns
-	if precision != nil {
-		return uint64(cellID.Parent(*precision))
+	if precision != 30 {
+		log.Print("Rounding down")
+		return [][]uint64{[]uint64{uint64(cellID.Parent(precision))}}
 	}
-	return uint64(cellID)
+	return [][]uint64{[]uint64{uint64(cellID)}}
 }
 
-// Polygon rapresent a geojson polygon geometry object
+// ToGeoJSON converts point to geoJSON
+func (p Point) ToGeoJSON(in [][]uint64) FeatureCollection {
+	ff := FeatureCollection{}
+	ff.Type = "FeatureCollection"
+	var features []Feature
+	for _, id := range in[0] {
+		// add center point
+		feature := cellIDToCenterPoint(id)
+		features = append(features, feature)
+		// add bounding box
+		feature = cellIDToPolygon(id)
+		features = append(features, feature)
+	}
+	ff.Feat = features
+	return ff
+}
 type Polygon struct {
 	Type string        `json:"type"`
 	C    []Coordinates `json:"coordinates"`
